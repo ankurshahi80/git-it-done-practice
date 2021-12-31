b877e07a6b350f599401ca2d0d7560b49817e910
ankurshahi80 <shahiankur80@gmail.com>
2021-12-31 09:11:25 -0500

    handle form submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,18 @@
 let userFormEl = document.querySelector("#user-form");
 let nameInputEl = document.querySelector("#username");
+
+const formSubmitHandler = function(event){
+  event.preventDefault();
+  // get value from input element
+  let username = nameInputEl.value.trim();
+  if(username) {
+    getUserRepos(username);
+    nameInputEl.value="";
+  } else {
+    alert("Please enter a Github username");
+  }
+};
+
 const getUserRepos = function (user) {
   // format the github api url
   let apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -12,4 +25,4 @@ const getUserRepos = function (user) {
   });
 };
 
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
