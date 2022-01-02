40a37f7fda9024d58446b90c4d116a4b824ebd74
ankurshahi80 <shahiankur80@gmail.com>
2022-01-01 22:09:20 -0500

    add link to github issue for repo with more than 30 issues.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 let issueContainerEl = document.querySelector("#issues-container");
+let limitWarningEl = document.querySelector("#limit-warning");
 
 const getRepoIssues = function(repo){
   let apiUrl = "https://api.github.com/repos/"+ repo + "/issues?direction=asc";
@@ -8,7 +9,11 @@ const getRepoIssues = function(repo){
       response.json().then(function(data){
         // pass response data to DOM function
         displayIssues(data);
-        console.log(data);
+
+        // check if api has paginated issues
+        if(response.headers.get("Link")){
+          displayWarning(repo);
+        }
       });
     }
     else {
@@ -52,6 +57,19 @@ const displayIssues = function(issues){
 
     issueContainerEl.appendChild(issueEl);
   }
+};
+
+const displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  let linkEl = document.createElement("a");
+  linkEl.textContent="See More Issues on GitHub.com";
+  linkEl.setAttribute("href","https://github.com/"+repo+"/issues");
+  linkEl.setAttribute("target","_blank");
+
+  // append to warninig container
+  limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
